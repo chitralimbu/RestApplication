91756c37aa93339beb5029c45d9d8ef2a32e5179
chitralimbu <limbu_nikkil@hotmail.co.uk>
2019-08-04 18:49:30 +0100

    REST Json and XML completed

--- src/com/user/data/UserDao.java
+++ src/com/user/data/UserDao.java
@@ -1,14 +1,30 @@
 package com.user.data;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserDao {
-	List<User> users = Arrays.asList(
-			new User(1, "Chitra", "Engineer"),
-			new User(2, "Ashmita","Nurse"),
-			new User(3, "Sam", "Sunday League")
-			);
+	List<User> users = setup();
+	
+	public static final String RESOURCE = "/home/dharanboi/Documents/RestData/users.csv";
+	
+	private List<User> setup() {
+		
+		try {
+			List<User> allUsers = Files.readAllLines(Paths.get(RESOURCE))
+					.stream()
+					.map(user -> user.split(","))
+					.map(user -> new User(Integer.parseInt(user[0]), user[1], user[2]))
+					.collect(Collectors.toList());
+			return allUsers;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 	public List<User> getAllUsers(){
 		return users;
@@ -19,6 +35,62 @@ public class UserDao {
 					.filter(s -> s.getName().equalsIgnoreCase(name))
 					.findAny()
 					.orElse(new User(0, "No Name Found", "Job Not Found"));
+	}
+	
+	public int addUser(User pUser) {
+		List<User> allUsers = setup();
+		allUsers.add(pUser);
+		replace(allUsers);
+		return 1;
+	}
+	
+	public int updateUser(User pUser) {
+		List<User> currentUsers = setup();
+		for(User user: currentUsers) {
+			if(user.getId() == pUser.getId()) {
+				System.out.println("Id is the same");
+				int index = currentUsers.indexOf(user);
+				currentUsers.set(index, pUser);
+				replace(currentUsers);
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	public int deleteUser(int id) {
+		System.out.println("ID passed through: " + id);
+		List<User> allUsers = setup();
+		for(User user: allUsers) {
+			System.out.println(user.getId());
+			if(user.getId() == id) {
+				int index = allUsers.indexOf(user);
+				allUsers.remove(index);
+				replace(allUsers);
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	public void replace(List<User> users) {
+		String stringUsers = users.stream()
+				.map(user -> String.format("%s,%s,%s", user.getId(), user.getName(), user.getProfession()))
+				.collect(Collectors.joining("\n"));
+		try {
+			Files.write(Paths.get(RESOURCE), stringUsers.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		UserDao userDao = new UserDao();
+		//userDao.addUser(new User(4, "Bob", "Singer"));
+		//userDao.deleteUser(3);
+		userDao.updateUser(new User(1, "Nikhil", "Writer"));
+		userDao.setup().stream()
+					.forEach(System.out::println);
 	}
 	
 }
---
+++ src/com/user/data/UserServiceJson.java
@@ -0,0 +1,51 @@
+package com.user.data;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/UserService/json")
+public class UserServiceJson {
+	
+	UserDao userDao = new UserDao();
+	
+	@Path("/all")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getUsersJson(){
+		return userDao.getAllUsers();
+	}
+	
+	@POST
+	@Path("/add/users")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createUser(User user) {
+		return Response.status(201).entity(userDao.addUser(user)).build();
+	}
+	
+	@PUT  
+	@Path("/update/users")  
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateUser(User user) {
+		return Response.status(200).entity(userDao.updateUser(user)).build();
+	}
+	
+	@DELETE
+	@Path("delete/users/{userId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response deleteUser(@PathParam("userId") int id) {
+		return Response.status(200).entity(userDao.deleteUser(id)).build();
+	}
+}
